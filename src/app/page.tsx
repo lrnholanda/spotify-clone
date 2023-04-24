94ab0ce9c7d515e4b4773de9ed825f9b38676522
Lorena Holanda <lrnholanda@gmail.com>
2023-04-24 20:14:28 -0300

    added news albuns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 
-import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight} from 'lucide-react'
+import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat2} from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -51,35 +51,99 @@ export default function Home() {
         </div>
         <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
         <div className='grid grid-cols-3 gap-4 mt-6'>
-          <div className='bg-white/10 rounded  flex items-center  gap-4 overflow-hidden'>
+          <a className='bg-white/10 group rounded  flex items-center  gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
             <Image src="/images/disco1.jpg" width={104} height={104} alt="capa do album"/>
               <strong>Salada de Frutas</strong>
-            </div>
-          <div className='bg-white/10 rounded  flex items-center  gap-4 overflow-hidden'>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play/>
+              </button>
+            </a>
+          <a className='bg-white/10 group rounded  flex items-center  gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/images/disco02.jpg" width={104} height={104} alt="capa do album"/>
           <strong>random songs</strong>
-          </div>
-          <div className='bg-white/10 rounded flex items-center  gap-4  overflow-hidden'>
+          <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play/>
+              </button>
+          </a>
+          <a className='bg-white/10 group rounded flex items-center  gap-4  overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/images/disco3.jpg" width={104} height={104} alt="capa do album"/>
           <strong>Sisyphus</strong>
-          </div>
-          <div className='bg-white/10 rounded  flex items-center  gap-4 overflow-hidden'>
+          <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play/>
+              </button>
+          </a>
+          <a className='bg-white/10 group rounded  flex items-center  gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/images/disco02.jpg" width={104} height={104} alt="capa do album"/>
           <strong>Transcender</strong>
-          </div>
-          <div className='bg-white/10 rounded  flex items-center  gap-4 overflow-hidden'>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play/>
+            </button>
+          </a>
+          <a className='bg-white/10 group rounded  flex items-center  gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/images/disco1.jpg" width={104} height={104} alt="capa do album"/>
           <strong>brasilidade</strong>
-          </div>
-          <div className='bg-white/10 rounded flex items-center  gap-4 overflow-hidden'>
+          <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play/>
+              </button>
+          </a>
+          <a className='bg-white/10 group rounded flex items-center  gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
           <Image src="/images/disco04.jpg" width={104} height={104} alt="capa do album"/>
           <strong>Nice Guys</strong>
-          </div>
+          <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play/>
+              </button>
+          </a>
+        </div>
+
+        <h1 className='font-semibold text-2xl mt-10'>Made for Lorena Holanda</h1>
+        <div className='grid grid-cols-8 gap-4 mt-4'>
+          <a className='bg-white/5 p-3 rounded-md  flex flex-col gap-2  hover:bg-white/10'>
+          <Image src="/images/disco1.jpg" className='w-full' width={120} height={120} alt="capa do album"/>
+          <strong className='font-semibold'>Daily Mix 1</strong>
+          <span className='text-sm text-zinc-500'>bôa, Weyes Blood, FKA twigs e mais</span>
+          </a>
+          <a className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src="/images/disco1.jpg" className='w-full' width={120} height={120} alt="capa do album"/>
+          <strong className='font-semibold'>Daily Mix 2</strong>
+          <span className='text-sm text-zinc-500'>PJ Harvey, Banda Eddie, Paramore e mais</span>
+          </a>
+          <a className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src="/images/disco1.jpg" className='w-full' width={120} height={120} alt="capa do album"/>
+          <strong className='font-semibold'>Daily Mix 3</strong>
+          <span className='text-sm text-zinc-500'>Fleet Foxes, Andrew Bird e mais</span>
+          </a>
+          <a className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src="/images/disco1.jpg" className='w-full' width={120} height={120} alt="capa do album"/>
+          <strong className='font-semibold'>Daily Mix 4</strong>
+          <span className='text-sm text-zinc-500'>Academia da Berlinda, BaianaSystem e mais</span>
+          </a>
+          <a className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src="/images/disco1.jpg" className='w-full' width={120} height={120} alt="capa do album"/>
+          <strong className='font-semibold'>Daily Mix 5</strong>
+          <span className='text-sm text-zinc-500'>Fleet Foxes, Andrew Bird e mais</span>
+          </a>
         </div>
       </main>
     </div>
     <footer className="bg-zinc-800 border-zinc-700 p-6">
-      footer
+      <div className='flex items-center gap-3'>
+      <Image src="/images/disco1.jpg" width={56} height={56} alt="capa do album"/>
+      <div className='flex flex-col'>
+        <strong className='font-normal'>Dorival</strong>
+        <span className='text-xs text-zinc-400'>Academia da Berlinda</span>
+      </div>
+      </div>
+      <div className='flex flex-col items-center'>
+        <div>
+        <Shuffle/>
+        <SkipBack/>
+        <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black ml-auto mr-8'>
+                <Play/>
+        </button>
+        <SkipForward/>
+        </div>
+      </div>
+      <div></div>
     </footer>
    </div>
   )
