05981b050a84eda2f0d11bf2f949b7c2769ce38b
Lorena Holanda <lrnholanda@gmail.com>
2023-04-24 10:43:24 -0300

    added buttons home search library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 
+import {Home as HomeIcon, Search, Library} from 'lucide-react'
+
 export default function Home() {
   return (
    <div className="h-screen flex flex-col">
     <div className="flex flex-1">
       <aside className="w-72 bg-zinc-950 p-6">
-        <nav>
-          <a href="">Home</a>
-          <a href="">Search</a>
-          <a href="">Your Library</a>
+        <nav className=' space-y-4'>
+          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+          <HomeIcon/>
+          Home
+          </a>
+          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+            <Search/>
+            Search</a>
+          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+            <Library/>
+            Your Library</a>
         </nav>
       </aside>
       <main className="flex-1 p-6">
